e9b1b73a38f81398c5d397d40fbd883879d31f46
sigh <wangximingde@163.com>
2014-08-14 12:44:28 +0800

    add 2 function

---
+++ js/jqueryApplication.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+    $('#SpriteAdd').on('click', function(){
+
+    })
+});
---
+++ js/model/goods.js
@@ -0,0 +1,6 @@
+function Goods(id, name, price, unit){
+    this.id= id;
+    this.name = name;
+    this.price = price;
+    this.unit = unit;
+}
