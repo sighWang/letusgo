2bead2160497079fdfbf741b0d0023647c169269
sigh <wangximingde@163.com>
2014-08-14 16:53:19 +0800

    use localstorage succed

--- js/jqueryApplication.js
+++ js/jqueryApplication.js
@@ -7,13 +7,15 @@ $('document').ready(function(){
     });
     var cartGoodsNumber = $('#cartGoodsNumber').text();
     cartGoodsNumber++;
+    goodslocalStorage();
+    var sprite = JSON.parse(localStorage.sprite);
     $('#cartGoodsNumber').text(cartGoodsNumber);
     $('#goodsList').append(
          '<div class="col-md-3">' +
             '<div class="thumbnail">' +
                 '<div class="caption">' +
-                     '<h3>' + +'</h3>' +
-                     '<p>单价：' + +' </p>' +
+                     '<h3>' + sprite.name +'</h3>' +
+                     '<p>单价：' + sprite.price +'(元) </p>' +
                      '<p><button id="AddCart"  class="btn btn-primary" role="button">加入购物车</button></p>' +
                 '</div>' +
            '</div>' +
--- js/model/localStorage.js
+++ js/model/localStorage.js
@@ -1,3 +1,8 @@
 function goodslocalStorage(){
-    localStorage.setItem('sprite',new Goods('雪碧', '3', '瓶'));
+    var sprite = new Goods('雪碧', '3', '瓶');
+    var lychee = new Goods('荔枝','15.00','斤');
+    var badminton = new Goods('羽毛球','4.50','个')
+    localStorage.setItem('sprite',JSON.stringify(sprite));
+    localStorage.setItem('lychee',JSON.stringify(lychee));
+    localStorage.setItem('badminton',JSON.stringify(badminton));
 }
